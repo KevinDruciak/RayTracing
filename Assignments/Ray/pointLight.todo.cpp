37509f8088b1dfaa532046eb06888db603fb1297
Kevin Druciak <kevintdruciak@gmail.com>
2020-10-14 19:25:14 -0400

    update

--- Assignments/Ray/pointLight.todo.cpp
+++ Assignments/Ray/pointLight.todo.cpp
@@ -17,6 +17,7 @@ Point3D PointLight::getAmbient( Ray3D ray , const RayShapeIntersectionInfo& iInf
 	////////////////////////////////////////////////////
 	const double distance = (iInfo.position - ray.position).length();
 	return Point3D(this->_ambient * iInfo.material->ambient / (this->_constAtten + this->_linearAtten * distance + this->_quadAtten * pow(distance, 2)));
+	//return iInfo.material->ambient * this->_ambient;
 }
 
 Point3D PointLight::getDiffuse( Ray3D ray , const RayShapeIntersectionInfo& iInfo ) const
@@ -24,9 +25,13 @@ Point3D PointLight::getDiffuse( Ray3D ray , const RayShapeIntersectionInfo& iInf
 	////////////////////////////////////////////////////
 	// Get the diffuse contribution of the light here //
 	////////////////////////////////////////////////////
-	const double distance = (iInfo.position - ray.position).length();
+	/*const double distance = (iInfo.position - ray.position).length();
 	const Point3D intensity = this->_diffuse / (this->_constAtten + this->_linearAtten * distance + this->_quadAtten * pow(distance, 2));
-	return iInfo.normal.dot(-this->_location) * iInfo.material->diffuse * intensity;
+	return iInfo.normal.dot(-this->_location) * iInfo.material->diffuse * intensity;*/
+	double strength = ((this->_location - iInfo.position).unit()).dot(iInfo.normal);
+	double dist = (this->_location - iInfo.position).length();
+	double intensity = 1 / (this->_constAtten + this->_linearAtten * dist + this->_quadAtten * dist * dist);
+	return iInfo.material->diffuse * this->_diffuse * intensity * (strength >= 0 ? strength : 0);
 }
 
 Point3D PointLight::getSpecular( Ray3D ray , const RayShapeIntersectionInfo& iInfo ) const
@@ -34,12 +39,27 @@ Point3D PointLight::getSpecular( Ray3D ray , const RayShapeIntersectionInfo& iIn
 	/////////////////////////////////////////////////////
 	// Get the specular contribution of the light here //
 	/////////////////////////////////////////////////////
-	double projection = (iInfo.normal).dot(ray.position - iInfo.position);
+	/*double projection = (iInfo.normal).dot(ray.position - iInfo.position);
 	Point3D reflection = 2.0 * projection * iInfo.normal - (ray.position - iInfo.position);
 	double angle = ray.direction.dot(reflection);
 	double distance = (this->_location - iInfo.position).length();
 	double intensity = 1.0 / (this->_constAtten + this->_linearAtten * distance + this->_quadAtten * pow(distance, 2));
-	return iInfo.material->specular * pow(angle, iInfo.material->specularFallOff) * intensity * this->_specular;
+	return iInfo.material->specular * pow(angle, iInfo.material->specularFallOff) * intensity * this->_specular;*/
+	Point3D toLight = (this->_location - iInfo.position).unit();
+	Point3D toCamera = (ray.position - iInfo.position).unit();
+	double projection = iInfo.normal.dot(toCamera);
+	Point3D reflection = iInfo.normal * projection + -(toCamera - iInfo.normal * projection);
+
+	double cos_angle = toLight.dot(reflection.unit());
+	if (cos_angle < 0)
+		return Point3D();
+
+	double strength = pow(cos_angle, iInfo.material->specularFallOff);
+
+	double dist = (this->_location - iInfo.position).length();
+	double intensity = 1 / (this->_constAtten + this->_linearAtten * dist + this->_quadAtten * dist * dist);
+
+	return iInfo.material->specular * this->_specular * intensity * strength;
 }
 
 bool PointLight::isInShadow( const RayShapeIntersectionInfo& iInfo , const Shape* shape ) const
@@ -58,8 +78,11 @@ Point3D PointLight::transparency( const RayShapeIntersectionInfo &iInfo , const 
 	//////////////////////////////////////////////////////////
 	// Compute the transparency along the path to the light //
 	//////////////////////////////////////////////////////////
-	THROW( "method undefined" );
-	return Point3D( 1. , 1. , 1. );
+	//Ray3D ray = Ray3D(iInfo.position, (this->_location - iInfo.position).unit());
+	//RayShapeIntersectionInfo tempInfo;
+	//if (shape.intersect(ray, tempInfo) > 0)
+	//	return tempInfo.material->transparent;
+	return Point3D();
 }
 
 void PointLight::drawOpenGL( int index , GLSLProgram * glslProgram ) const
--- Assignments/Ray/scene.todo.cpp
+++ Assignments/Ray/scene.todo.cpp
@@ -4,10 +4,12 @@
 
 using namespace Ray;
 using namespace Util;
+
 double clamp(double color)
 {
 	return std::max(0.0, std::min(1.0, color));
 }
+
 ///////////
 // Scene //
 ///////////
@@ -29,8 +31,17 @@ bool Scene::Refract( Point3D v , Point3D n , double ir , Point3D& refract )
 	//////////////////
 	// Refract here //
 	//////////////////
-	THROW( "method undefined" );
-	return false;
+	/*
+	Point3D incidence = -v.unit();
+	Point3D normal = n.unit();
+	double sin_incidence = n.CrossProduct(v).length() / ir;
+
+	if (sin_incidence > 1) return false;
+	double theta_reflected = asin(sin_incidence);
+
+	refract = normal * ((1 / ir) * incidence.dot(normal) - cos(theta_reflected)) - incidence * (1 / ir);
+	return true;*/
+	return true;
 }
 
 Point3D Scene::getColor( Ray3D ray , int rDepth , Point3D cLimit )
@@ -38,14 +49,14 @@ Point3D Scene::getColor( Ray3D ray , int rDepth , Point3D cLimit )
 	////////////////////////////////////////////////
 	// Get the color associated with the ray here //
 	////////////////////////////////////////////////
-		//TODO: modify maybe?
+
 	RayShapeIntersectionInfo iInfo;
 	const double result = this->intersect(ray, iInfo, BoundingBox1D(), std::function< bool(double) >());
 	if (result == Infinity) {
 		return Point3D();
 	}
 
-	Point3D color;
+	/*Point3D color;
 	for (auto* light : (*this)._globalData.lights)
 	{
 		color += light->getAmbient(ray, iInfo);
@@ -57,6 +68,124 @@ Point3D Scene::getColor( Ray3D ray , int rDepth , Point3D cLimit )
 	color[0] = clamp(color[0]);
 	color[1] = clamp(color[1]);
 	color[2] = clamp(color[2]);
+
+	return color;*/
+	// Rays "pass through" the backsides of shapes 
+	/*if (ray.direction.dot(iInfo.normal) > 0) {
+		ray.position = iInfo.position + ray.direction * 0.00001;
+		return getColor(ray, rDepth, cLimit);
+	}
+
+	rDepth--;
+	Point3D color = iInfo.material->ambient + iInfo.material->emissive;
+
+	// Add reflection
+	int cutOffReflect = 1;
+	for (int i = 0; i < 3; i++)
+		if (iInfo.material->specular[i] > cLimit[i])
+			cutOffReflect = 0;
+
+
+	if (rDepth > 0 && !cutOffReflect) {
+		Ray3D reflect;
+		reflect.direction = Reflect(ray.direction.unit(), iInfo.normal.unit());
+		reflect.position = iInfo.position + reflect.direction * 0.00001;
+		color += (Point3D(1,1,1) - iInfo.material->transparent) * iInfo.material->specular * this->getColor(reflect, rDepth, cLimit / iInfo.material->specular);
+	}
+
+	for (auto* light : (*this)._globalData.lights) {
+		//RayLight& l = *this->lights[i];
+		
+		Point3D t = Point3D(1,1,1);
+		for (int j = 0; j < this->primitiveNum(); j++) {
+			t *= light->transparency(iInfo, group->shapes[j], cLimit / iInfo.material->transparent);
+		}
+
+		if (iInfo.material->tex == NULL) {
+			color += t * l.getDiffuse(camera->position, iInfo);
+		}
+		else {
+			Pixel texVal = Pixel(iInfo.material->tex->img->BilinearSample(iInfo.texCoordinate[0], iInfo.texCoordinate[1]));
+			color += t * Point3D(texVal.r, texVal.g, texVal.b);
+		}
+		color += t * l.getSpecular(camera->position, iInfo);
+
+	}
+
+	for (int i = 0; i < 3; i++) {
+		if (color[i] < 0) color[i] = 0;
+		else if (color[i] > 1) color[i] = 1;
+	}
+
+	return color;*/
+/*
+	// Add refraction
+	// int cutOffRefract = 1;
+	// for (int i = 0; i < 3; i++)
+	// 	if (iInfo.material->transparent[i] > cLimit[i])
+	// 		cutOffRefract = 0;
+
+	// int belowCriticalAngle = 0;
+	// if (rDepth > 0 && !cutOffRefract) {
+	// 	Ray3D refract;
+	// 	belowCriticalAngle = Refract(ray.direction.unit(), iInfo.normal.unit(), iInfo.material->ir, refract.direction);
+	// 	if (belowCriticalAngle) {
+	// 		refract.position = iInfo.position + refract.direction * 0.00001;
+	// 		color += iInfo.material->transparent * this->getColor(refract, rDepth, cLimit / iInfo.material->transparent);
+	// 	}
+	// }*/
+	if (ray.direction.dot(iInfo.normal) > 0) {
+		ray.position = iInfo.position + ray.direction * 0.00001;
+		return getColor(ray, rDepth, cLimit);
+	}
+
+	rDepth--;
+	Point3D color = iInfo.material->ambient + iInfo.material->emissive;
+
+	// Add reflection
+	int cutOffReflect = 1;
+	for (int i = 0; i < 3; i++)
+		if (iInfo.material->specular[i] > cLimit[i])
+			cutOffReflect = 0;
+
+
+	if (rDepth > 0 && !cutOffReflect) {
+		Ray3D reflect;
+		reflect.direction = Reflect(ray.direction.unit(), iInfo.normal.unit());
+		reflect.position = iInfo.position + reflect.direction * 0.00001;
+		color += iInfo.material->specular * this->getColor(reflect, rDepth, cLimit / iInfo.material->specular);
+	}
+	for (auto* light : (*this)._globalData.lights) {
+
+	//for (int i = 0; i < this->lightNum; i++) {
+	//	RayLight& l = *this->lights[i];
+
+		//Point3D t = Point3D(1,1,1);
+		//for (int j = 0; j < group->shapeNum(); j++) {
+		//	t *= l.transparency(iInfo, group->shapes[j], cLimit / iInfo.material->transparent);
+		//}
+		//color += light->getAmbient(ray, iInfo);
+
+		//if (iInfo.material->tex == NULL) {
+		color += light->getDiffuse(ray, iInfo) * light->isInShadow(iInfo, this);
+		//}
+		//else {
+		//	Pixel texVal = Pixel(iInfo.material->tex->img->BilinearSample(iInfo.texCoordinate[0], iInfo.texCoordinate[1]));
+		//	color += t * Point3D(texVal.r, texVal.g, texVal.b);
+		//}
+		//color += light->getAmbient(ray, iInfo);
+		color += light->getSpecular(ray, iInfo) * light->isInShadow(iInfo, this);
+
+	}
+
+	for (int i = 0; i < 3; i++) {
+		if (color[i] < 0) color[i] = 0;
+		else if (color[i] > 1) color[i] = 1;
+	}
+	//color += iInfo.material->emissive;
+	//color[0] = clamp(color[0]);
+	//color[1] = clamp(color[1]);
+	//color[2] = clamp(color[2]);
 
 	return color;
 }
