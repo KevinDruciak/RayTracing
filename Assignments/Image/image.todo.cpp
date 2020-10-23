c21a94b1d7b4b8f01ab75ee8a0398d78952079aa
Kevin Druciak <kevintdruciak@gmail.com>
2020-10-23 16:11:08 -0400

    sampling for ray

--- Assignments/Image/image.todo.cpp
+++ Assignments/Image/image.todo.cpp
@@ -7,6 +7,8 @@
 using namespace Util;
 using namespace Image;
 
+
+
 /////////////
 // Image32 //
 /////////////
@@ -215,14 +217,35 @@ Image32 Image32::crop( int x1 , int y1 , int x2 , int y2 ) const
 	THROW( "method undefined" );
 	return Image32();
 }
-
+bool checkBounds(double val, int size)
+{
+	return val > size - 1 ? false : val < 0 ? false : true;
+}
 Pixel32 Image32::nearestSample( Point2D p ) const
 {
 	//////////////////////////////
 	// Do nearest sampling here //
 	//////////////////////////////
-	THROW( "method undefined" );
-	return Pixel32();
+	Pixel32 newPix;
+	//std::cout << p[0] << " " << p[1] << std::endl;
+
+	Point2D p1 = Point2D(p[0] * (double)this->_width, p[1] * (double)this->_height);
+	//std::cout << p1[0] << " " << p1[1] << std::endl;
+	//std::cout <<  " " << std::endl;
+
+	if (!checkBounds(floor(p1[0] + 0.5), _width) || !checkBounds(floor(p1[1] + 0.5), _height)) {
+		newPix.a = 0;
+		newPix.r = 0;//(*this)(floor(p1[0] + 0.5), floor(p1[1] + 0.5)).r;
+		newPix.b = 0;//(*this)(floor(p1[0] + 0.5), floor(p1[1] + 0.5)).b;
+		newPix.g = 0;//(*this)(floor(p1[0] + 0.5), floor(p1[1] + 0.5)).g;
+	} else {
+		newPix.a = (*this)(floor(p1[0] + 0.5), floor(p1[1] + 0.5)).a;
+		newPix.r = (*this)(floor(p1[0] + 0.5), floor(p1[1] + 0.5)).r;
+		newPix.b = (*this)(floor(p1[0] + 0.5), floor(p1[1] + 0.5)).b;
+		newPix.g = (*this)(floor(p1[0] + 0.5), floor(p1[1] + 0.5)).g;
+	}
+
+	return newPix;
 }
 
 Pixel32 Image32::bilinearSample( Point2D p ) const
@@ -230,8 +253,92 @@ Pixel32 Image32::bilinearSample( Point2D p ) const
 	///////////////////////////////
 	// Do bilinear sampling here //
 	///////////////////////////////
-	THROW( "method undefined" );
-	return Pixel32();
+
+	Pixel32 newPix;
+	double u = p[0] * (double)this->_width;
+	double v = p[1] * (double)this->_height;
+
+	double u1 = floor(u);
+	double u2 = u1 + 1;
+	double v1 = floor(v);
+	double v2 = v1 + 1;
+	double du = u - u1;
+	double dv = v - v1;
+
+	Pixel32 bl;
+	Pixel32 br;
+	Pixel32 tl;
+	Pixel32 tr;
+
+	if (!checkBounds(u1, _width) || !checkBounds(v1, _height)) {
+		bl.a = 0;
+		bl.r = 0;
+		bl.b = 0;
+		bl.g = 0;
+	}
+	else {
+		bl.a = (*this)(u1, v1).a;
+		bl.r = (*this)(u1, v1).r;
+		bl.b = (*this)(u1, v1).b;
+		bl.g = (*this)(u1, v1).g;
+	}
+
+	if (!checkBounds(u2, _width) || !checkBounds(v1, _height)) {
+		br.a = 0;
+		br.r = 0;
+		br.b = 0;
+		br.g = 0;
+	}
+	else {
+		br.a = (*this)(u2, v1).a;
+		br.r = (*this)(u2, v1).r;
+		br.b = (*this)(u2, v1).b;
+		br.g = (*this)(u2, v1).g;
+	}
+
+	if (!checkBounds(u1, _width) || !checkBounds(v2, _height)) {
+		tl.a = 0;
+		tl.r = 0;
+		tl.b = 0;
+		tl.g = 0;
+	}
+	else {
+		tl.a = (*this)(u1, v2).a;
+		tl.r = (*this)(u1, v2).r;
+		tl.b = (*this)(u1, v2).b;
+		tl.g = (*this)(u1, v2).g;
+	}
+
+	if (!checkBounds(u2, _width) || !checkBounds(v2, _height)) {
+		tr.a = 0;
+		tr.r = 0;
+		tr.b = 0;
+		tr.g = 0;
+	}
+	else {
+		tr.a = (*this)(u2, v2).a;
+		tr.r = (*this)(u2, v2).r;
+		tr.b = (*this)(u2, v2).b;
+		tr.g = (*this)(u2, v2).g;
+	}
+
+	double a = bl.a * (1 - du) + br.a * du;
+	double b = tl.a * (1 - du) + tr.a * du;
+	newPix.a = a * (1 - dv) + b * dv;
+
+	a = bl.r * (1 - du) + br.r * du;
+	b = tl.r * (1 - du) + tr.r * du;
+	newPix.r = a * (1 - dv) + b * dv;
+
+	a = bl.g * (1 - du) + br.g * du;
+	b = tl.g * (1 - du) + tr.g * du;
+	newPix.g = a * (1 - dv) + b * dv;
+
+	a = bl.b * (1 - du) + br.b * du;
+	b = tl.b * (1 - du) + tr.b * du;
+	newPix.b = a * (1 - dv) + b * dv;
+
+	return newPix;
 }
 
 Pixel32 Image32::gaussianSample( Point2D p , double variance , double radius ) const
